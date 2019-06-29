d3da025c2660861fe8ad13d8e540207bfaa6a93f
Neo <kkokdari@foxmail.com>
2019-06-29 23:12:43 +0800

    onShow

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -1,4 +1,5 @@
 // miniprogram/pages/me/me.js
+const util = require('../../utils/util')
 Page({
 
   /**
@@ -26,12 +27,21 @@ Page({
       title: 'This function is not open yet.'
     })
   },
-
+// onShow 生命周期函数表示页面被打开时触发的功能，onLoad 函数表示页面在加载时触发的功能。也就是说，前者在页面加载、反复打开的过程中，会触发多次。而后者则只会被触发一次。
+  onShow: function () {
+    util.getUserInfo().then(userInfo => {
+      this.setData({
+        userInfo
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
   },
+
+  
 
 })
--- miniprogram/utils/util.js
+++ miniprogram/utils/util.js
@@ -2,5 +2,30 @@ module.exports = {
   // make price with 2 digits
   formatPrice(price) {
     return parseFloat(Math.round(price * 100) / 100).toFixed(2)
+  },
+// 加入一项功能：系统在到登录页面后，先检查一下我们的授权信息，如果用户的授权信息已经显示被同意，那我们直接获取用户的昵称和头像，无需需要触发点击登录按钮实现。
+  getUserInfo(){
+    return new Promise((resolve,reject)=> {
+      wx.getSetting({
+        success(res) {
+          if (res.authSetting['scope.userInfo'] === false){
+            //reject
+            reject()
+          }
+          //authorized
+          else{
+            wx.getUserInfo({
+              success(res) {
+                const userInfo = res.userInfo
+                resolve(userInfo)
+              }
+            })
+          }
+        }
+      })
+    })
   }
 }
+
+
+
