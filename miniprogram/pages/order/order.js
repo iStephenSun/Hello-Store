174d78a46bd5d15b395032088c240d4515ee1570
Neo <kkokdari@foxmail.com>
2019-07-05 16:56:27 +0800

    Page-Order html

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -8,6 +8,41 @@ Page({
    */
   data: {
     userInfo:null,
+    orderList: [{
+      id: 0,
+      productList: [{
+        count: 1,
+        image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product1.jpg',
+        name: 'Product 1',
+        price: "50.50",
+      }]
+    },
+    {
+      id: 1,
+      productList: [{
+        count: 1,
+        image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product2.jpg',
+        name: 'Product 2',
+        price: "40.10",
+      },
+      {
+        count: 1,
+        image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product3.jpg',
+        name: 'Product 3',
+        price: "30.50",
+      }
+      ]
+    },
+    {
+      id: 2,
+      productList: [{
+        count: 2,
+        image: 'https://s3.cn-north-1.amazonaws.com.cn/u-img/product4.jpg',
+        name: 'Product 4',
+        price: "70.40",
+      }]
+    }
+    ],
   },
 
   onTapLogin(event){
@@ -26,7 +61,15 @@ Page({
         userInfo
       })
     })
-  },
+
+    // this.data.orderList.forEach(order => {
+    //   order.productList.forEach(product => product.price = util.priceFormat(product.price))
+    // })
+
+    // this.setData({
+    //   orderList:this.data.orderList
+    // })
+  }
 
  
 })
